f93e501bcd431510b09c9e0f371cbf4bc80124be
acandmishra <acandmishra@gmail.com>
2024-05-06 15:07:43 +0530

    Commit 3
    Task done ->
    # Replaced print statements by log function , which takes a string to be displayed, log function is available in the "dart:developer"
    #  In the builder function of Home Page we returned the notes view if the email was verified , if not we returned the verify email view and if the user was null we returned login view
    # Created enumeration items for the popup menu
    # Added leading button in the appbar of notes view in the actions property of the appbar which takes a list of widgets to be displayed after the title text
    # Added logout item in the pop up menu using the itembuilder property of pop up menu
    # Then created a function to show dialog box for sign out confirmation
    # Under the onSelected property of the popup menu we used switch case over the enumeration of popup items
    # we created the logic to sign out the user and move to the login page once the user confirmed the sign out
    
    Future Task->
    # Currently after returning to the login page after logging out of the notes view , if we try to login again , we are not navigated to the notes view ...... This will be fixed in next commit

--- lib/main.dart
+++ lib/main.dart
@@ -5,54 +5,130 @@ import 'package:notes_app/firebase_options.dart';
 import 'package:notes_app/views/login_view.dart';
 import 'package:notes_app/views/register_view.dart';
 import 'package:notes_app/views/verify_email_view.dart';
+import 'dart:developer' as devtools show log;
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp( MaterialApp(
+  runApp(
+    MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
       home: const HomePage(),
       routes: {
-        "/login/": (context)=> const LoginView(),
-        "/register/": (context)=> const RegisterView(),
-      }
-    )
-    );
+        "/login/": (context) => const LoginView(),
+        "/register/": (context) => const RegisterView(),
+      },
+    ),
+  );
 }
 
 class HomePage extends StatelessWidget {
-  const HomePage ({super.key});
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-        future:Firebase.initializeApp(
-            options:DefaultFirebaseOptions.currentPlatform,
-          ),
-        builder:(context, snapshot) {
-          switch(snapshot.connectionState){
-            case ConnectionState.done:
-              final user=(FirebaseAuth.instance.currentUser);
-              if (user!=null){
-                if (user.emailVerified==true){
-                  print("Email is verified");
-                } else{
-                  return const VerifyEmailView();
-                }
-              }else{
-                return const LoginView();
+      future: Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform,
+      ),
+      builder: (context, snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.done:
+            final user = (FirebaseAuth.instance.currentUser);
+            if (user != null) {
+              if (user.emailVerified == true) {
+                devtools.log("Email is verified");
+                return const NotesView();
+              } else {
+                return const VerifyEmailView();
               }
-              return const Text("Done");
+            } else {
+              return const LoginView();
+            }
+
           default:
             return const CircularProgressIndicator();
-          }
-        },
-      );
+        }
+      },
+    );
   }
 }
 
+enum MenuAction { logout }
 
+class NotesView extends StatefulWidget {
+  const NotesView({super.key});
+
+  @override
+  State<NotesView> createState() => _NotesViewState();
+}
+
+class _NotesViewState extends State<NotesView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: const Text("Notes"),
+          backgroundColor: const Color.fromARGB(255, 253, 97, 128),
+          actions: [
+            PopupMenuButton<MenuAction>(
+              onSelected: (value) async {
+                switch (value) {
+                  case (MenuAction.logout):
+                    // we are awaiting to let use click any button in the dialog box
+                    final shouldLogout = await showLogOutDialog(context);
+                    devtools.log(shouldLogout.toString());
+                    if (shouldLogout) {
+                      await FirebaseAuth.instance.signOut();
+                      Navigator.of(context)
+                          .pushNamedAndRemoveUntil("/login/", (_) => false);
+                    }
+                }
+              },
+              itemBuilder: (context) {
+                return const [
+                  PopupMenuItem<MenuAction>(
+                    value: MenuAction.logout,
+                    child: Text("Logout"),
+                  ),
+                ];
+              },
+            ),
+          ]),
+      body: const Text("Hello World"),
+    );
+  }
+}
+
+Future<bool> showLogOutDialog(BuildContext context) {
+  // This function is a Future because we will be waiting for user to press any one of the button (cancel/log out) or select none
+  // based on what is pressed action will be performed
+  return showDialog<bool>(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        title: const Text("Alert"),
+        content: const Text("Do you want to sign out?"),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop(false);
+            },
+            child: const Text("Cancel"),
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop(true);
+            },
+            child: const Text("Logout"),
+          ),
+        ],
+      );
+    },
+  ).then((value) => value ?? false);
+  // This .then() functions says that , it will grab the value being returned by the function it is applied to and will check if the value is null or not ,
+  // if the value is not null it will return the function's value else it will return false
+}
